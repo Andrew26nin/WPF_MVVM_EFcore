8daaf51284040812e283e9a379057e0c150b74ae
Andrew26nin <tran26@mail.ru>
2019-02-12 22:39:13 +0300

    BuisnessLogic

---
+++ BuisnessLogic/BuisnessLogic.cs
@@ -0,0 +1,129 @@
+﻿using Data.DataRepository;
+using Data.DataRepository.Interface;
+using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLogic
+{
+    public class BuisnessLogic : IBuisnessLogic
+    {
+
+        private IClientRepository _clientRepository;
+        private IOrderRepository _orderRepository;
+
+
+        #region Конструктор
+        /// <summary>
+        /// Конструкторы класса бизнес-логики
+        /// </summary>
+        public BuisnessLogic()
+        {
+            _clientRepository = new ClientRepository();
+            _orderRepository = new OrderRepository();
+        }
+
+        public BuisnessLogic(IClientRepository clientRepository, IOrderRepository orderRepository)
+        {
+            _clientRepository = clientRepository;
+            _orderRepository = orderRepository;
+        }
+        #endregion
+
+        #region CRUD Client
+        public IList<Client> GetAllClients()
+        {
+            return _clientRepository.GetAll();
+        }
+
+        /// <summary>
+        /// Валидация клиента
+        /// </summary>
+        /// <param name="clients">массив клиентов</param>
+        /// <returns>Возвращает флаг проверки</returns>
+        private bool ValidationClient(Client[] clients)
+        {
+            foreach (var client in clients)
+            {
+                if (client.Name == null || client.Address == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public void AddClient(params Client[] clients)
+        {
+            if (ValidationClient(clients) == false)
+            {
+                return;
+            }
+            _clientRepository.InsertMany(clients);
+        }
+
+        public void UpdateClient(params Client[] clients)
+        {
+            if (ValidationClient(clients) == false)
+            {
+                return;
+            }
+            _clientRepository.Update(clients);
+        }
+
+
+        public void RemoveClient(params Client[] clients)
+        {
+            _clientRepository.Delete(clients);
+        }
+
+        #endregion
+
+        #region CRUD Order
+
+        public IList<Order> GetOrderByClientId(int ClientId)
+        {
+            return _orderRepository.GetList(x => x.ClientId.Equals(ClientId));
+        }
+
+        private bool ValidationOrder(Order order)
+        {
+            if (order.Description == null)
+                return false;
+            return true;
+        }
+
+
+        public void AddOrder(Order order)
+        {
+            if (ValidationOrder(order) == false)
+                return;
+            _orderRepository.InsertMany(order);
+        }
+
+
+        public void UpdateOrder(Order order)
+        {
+            if (ValidationOrder(order) == false)
+                return;
+            _orderRepository.Update(order);
+        }
+
+
+        public void RemoveOrder(Order order)
+        {
+            _orderRepository.Delete(order);
+        }
+
+
+
+
+        #endregion
+
+
+    }
+}
---
+++ BuisnessLogic/IBuisnessLogic.cs
@@ -0,0 +1,24 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLogic
+{
+    public interface IBuisnessLogic
+    {
+        IList<Client> GetAllClients();
+        //Cities GetCitiesByName(string citiesName);
+        void AddClient(params Client[] client);
+        void UpdateClient(params Client[] client);
+        void RemoveClient(params Client[] client);
+
+        //IList<Streets> GetStreetssByCitiesName(string citiesName);
+        IList<Order> GetOrderByClientId(int ClientId);
+        void AddOrder(Order order);
+        void UpdateOrder(Order order);
+        void RemoveOrder(Order order);
+    }
+}
